038eded3e670a2b8160bff1f29d7024393380172
Aman Verma <amanvermaspace@gmail.com>
2024-06-13 17:38:17 +0530

    added initial configuration for shadcnui and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TransporterDataTable from "./Components/TransporterDataTable.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <TransporterDataTable></TransporterDataTable>
+    </div>
+  );
+};
+
+export default App;
